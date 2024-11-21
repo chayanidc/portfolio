8f77f85991d46876d604bfd99fb433c7078b274c
Chayanid.c <104764972+chayanidc@users.noreply.github.com>
2024-11-22 06:51:28 +0700

    Add files via upload

---
+++ Restaurant Data Analysis/Restaurant Orders Analysis.sql
@@ -0,0 +1,24 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE menu_items (
+  menu_item_id SMALLINT NOT NULL,
+  item_name VARCHAR(45),
+  category VARCHAR(45),
+  price DECIMAL(5,2),
+  PRIMARY KEY (menu_item_id)
+);
+CREATE TABLE order_details (
+  order_details_id SMALLINT NOT NULL,
+  order_id SMALLINT NOT NULL,
+  order_date DATE,
+  order_time TIME,
+  item_id SMALLINT,
+  PRIMARY KEY (order_details_id)
+);
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
